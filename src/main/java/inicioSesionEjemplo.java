85452eefdbefbae837204a668b194ee14ca1e5cc
GonzaloNHC <101850889+GonzaloNHC@users.noreply.github.com>
2022-05-31 20:37:45 -0400

    Add files via upload

--- src/main/java/inicioSesionEjemplo.java
+++ src/main/java/inicioSesionEjemplo.java
@@ -7,23 +7,23 @@ public class inicioSesionEjemplo {
         String us ="Gonzalo", pass="1234h";
         String user, password;
 
-        System.out.print("ingrese su usuario");
+        System.out.println("ingrese su usuario");
         user=x.nextLine();
         System.out.println("Ingrese su contraseña");
         password=x.nextLine();
 
         int intentos=0;
+        intentos++;
         if(us.equals(user)&& pass.equals(password)){
             System.out.println("Bienvenido al sistema");
+            GetFitAvance gtf = new GetFitAvance();
+            gtf.menu();
+
         }
 
         while (pass.equals(password)==false&&intentos<3){
-            System.out.println("Hay un error en los datos, intete nuevamente");
+            System.out.println("Hay un error en los datos, intente nuevamente");
         }
 
     }
-
-
-
-
 }
